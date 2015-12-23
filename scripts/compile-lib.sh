0f28558aa72a5ce128cf0bb36359ca48aaf1cb39
Marcus Westin <marcus.westin@gmail.com>
2015-12-23 10:00:14 -0500

    Start minifying the build

--- scripts/compile-lib.sh
+++ scripts/compile-lib.sh
@@ -4,3 +4,6 @@ export PATH=../node_modules/.bin:$PATH
 
 browserify ../es6/index.es6 --outfile ../autoreact.build.js --no-bundle-external \
 	--extension .es6 -t babelify
+
+browserify ../es6/index.es6 --outfile ../autoreact.min.js --no-bundle-external  \
+	--extension .es6 -t babelify --presets es2015 -t uglifyify
