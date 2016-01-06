5a663992b8ee2f560dbcb35f5ed554db0e1ef39c
Marcus Westin <marcus.westin@gmail.com>
2016-01-06 11:00:52 -0500

    Give autoreact a named budo port

--- scripts/open-example.sh
+++ scripts/open-example.sh
@@ -2,5 +2,5 @@ set -e
 cd $(cd `dirname ${BASH_SOURCE[0]}` && pwd -P)
 export PATH=../node_modules/.bin:$PATH
 
-budo ../examples/exampleApp.jsx --live --open --title "autoreact example" -- -d \
+budo ../examples/exampleApp.jsx --port 9902 --live --open --title "autoreact example" -- -d \
 	-t babelify --presets react
