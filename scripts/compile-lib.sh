7b735b0c02cc821bd743d32c715e1b64d2a5d432
Marcus Westin <marcus.westin@gmail.com>
2017-05-29 12:03:30 -0400

    Fix script for pathnames with spaces

--- scripts/compile-lib.sh
+++ scripts/compile-lib.sh
@@ -1,5 +1,5 @@
 set -e
-cd $(cd `dirname ${BASH_SOURCE[0]}` && pwd -P)
+cd "$(cd `dirname "${BASH_SOURCE[0]}"` && pwd -P)"
 export PATH=../node_modules/.bin:$PATH
 
 browserify ../src/autoreact.js --outfile ../dist/autoreact.min.js --no-bundle-external  \
--- scripts/open-example.sh
+++ scripts/open-example.sh
@@ -1,5 +1,5 @@
 set -e
-cd $(cd `dirname ${BASH_SOURCE[0]}` && pwd -P)
+cd "$(cd `dirname "${BASH_SOURCE[0]}"` && pwd -P)"
 export PATH=../node_modules/.bin:$PATH
 
 budo ../examples/exampleApp.js --port 9902 --live --open --title "AutoReact example" -- -d \
